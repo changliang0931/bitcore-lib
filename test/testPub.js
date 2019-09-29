2e76e601e1cd795ab0f0f4c946f08e4794df6ecf
changliang0931 <changliang0931@126.com>
2019-09-29 17:19:33 +0800

    testPub suppot xpub ypub zpub

--- test/testPub.js
+++ test/testPub.js
@@ -1,11 +1,51 @@
 'use strict';
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
 
-var bitcore = require('../index');
+var bitcore = require('..');
 var async = require('async');
 var HDPublicKey = bitcore.HDPublicKey;
 var Address = bitcore.Address;
 var Networks = bitcore.Networks;
-var bech32 = require('bech32');
+describe('deriveAddressByAccount', function() {
+  var xpub = "xpub6BgST3rEwmcjwe8cHxm8Z9cGcmTAJ5faVbu3JjD3tRkp49uN8RVpi1HRYrhzRcoXBdNRxgbxy4t3mE7Qo86Fcr57XWfdujhf822dL3yiq3p";
+  it('xpub: '+xpub+ ' \n deriveChild \n m\/44\'\/0\'\/0\'\/0\/0	12eENMysYzSqJVKP1xH6BERLio1hjobngY \n m\/44\'\/0\'\/0\'\/0\/1	1G28pJ967FkJhtznE163SK8yFAFsS2MpHJ', function() {
+    var xresults;
+    //m/44'/0'/0'/0/0	12eENMysYzSqJVKP1xH6BERLio1hjobngY
+    //m/44'/0'/0'/0/1	1G28pJ967FkJhtznE163SK8yFAFsS2MpHJ
+    deriveAddressByxpubAccount(xpub, 0, 2, 0, function (err,child) {
+      xresults= child;
+    });
+    expect(xresults[0].address).to.equal('12eENMysYzSqJVKP1xH6BERLio1hjobngY','xpub error');
+    expect(xresults[1].address).to.equal('1G28pJ967FkJhtznE163SK8yFAFsS2MpHJ','xpub error');
+  });
+
+
+  var ypub = "ypub6XExBxCDSHCpNF1Anpo97o57wz76fu6eqZFmcWCf2TKMYXyZ2S2Zt47xi4A89yjawNLWfzKQSQNQmfsDLL2CgaRY9p7PuFYBdSo9Z4K7GQq";
+  it('ypub: '+ypub+ '\n deriveChild \n m\/49\'\/0\'\/0\'\/0\/0	39nymRHzhPfw3SLwMZBRNxpFaeRirj9GUq \n m\/49\'\/0\'\/0\'\/0\/1	3LhwrAAm7E9zYXQJQM9cApv48YLYziZZaB', function() {
+    //m/49'/0'/0'/0/0	39nymRHzhPfw3SLwMZBRNxpFaeRirj9GUq
+    //m/49'/0'/0'/0/1	3LhwrAAm7E9zYXQJQM9cApv48YLYziZZaB
+    var yresults;
+    deriveAddressByypubAccount(ypub, 0, 2, 0, function (err,child) {
+      yresults= child;
+    });
+    expect(yresults[0].address).to.equal('39nymRHzhPfw3SLwMZBRNxpFaeRirj9GUq','ypub error');
+    // expect(yresults).to.include({address:'3LhwrAAm7E9zYXQJQM9cApv48YLYziZZaB'},);
+  });
+
+  var zpub = "zpub6r45L5aNdPMUpfdhpoBFvjjfaDSfQTcopJveBDzQmMGNfRW8dJdTYZDBmctH7y9F3N5Tajk8Z2xhdMYMwXKqWrzon9hVzXJ3GbrgP7Gqy26";
+  it('zpub: '+zpub+ '\n deriveChild \n m\/84\'\/0\'\/0\'\/0\/0	bc1q4swj9y83ed7jgf3ytc97wqs4zd4d907fmmy5gw \n m\/84\'\/0\'\/0\'\/0\/1	bc1qlygc98e8v64nffc7klek3qnql5kqrq6scuxrwp', function() {
+    //m/84'/0'/0'/0/0	bc1q4swj9y83ed7jgf3ytc97wqs4zd4d907fmmy5gw	
+    //m/84'/0'/0'/0/1	bc1qlygc98e8v64nffc7klek3qnql5kqrq6scuxrwp
+    var zresults;
+    deriveAddressByzpubAccount(zpub, 0, 2, 0, function (err,child) {
+      zresults= child;
+    });
+    expect(zresults[0].address).to.equal('bc1q4swj9y83ed7jgf3ytc97wqs4zd4d907fmmy5gw','zpub error');
+    expect(zresults[1].address).to.equal('bc1qlygc98e8v64nffc7klek3qnql5kqrq6scuxrwp','zpub error');
+  });
+});
 //https://iancoleman.io/bip39/
 /**
  * @description 根据xpub及地址index派生新change地址
@@ -24,7 +64,7 @@ var deriveAddressByxpubAccount = function (account, index, number, change, callb
       var child = hdPublicKey.deriveChild(change).deriveChild(index);
       var address = new Address(child.publicKey, Networks.livenet);
       results.push({ address: address.toString(), index: index, account: account, change: change });
-      console.log({ address: address.toString(), index: index, account: account, change: change });
+      // console.log({ address: address.toString(), index: index, account: account, change: change });
       index = index + 1;
       next(null);
     }, function (err) {
@@ -98,12 +138,3 @@ var deriveAddressByzpubAccount = function (account, index, number, change, callb
     return callback(error);
   }
 };
-var xpub = "";
-deriveAddressByxpubAccount(xpub, 0, 2, 0, function () {
-});
-var ypub = "";
-deriveAddressByypubAccount(ypub, 0, 2, 0, function () {
-});
-var zpub = "";
-deriveAddressByzpubAccount(zpub, 0, 2, 0, function () {
-});
